5f9592bd25ead5d8df4eaa8b804a865939471b8f
RoboCop-11 <itsleooo20@gmail.com>
2025-04-21 00:18:09 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Name: Manan
+// PRN: 23070126156
+// Batch: AIML-B3
+
+public class Main {
+    public static void main(String[] args) {
+        StudentOperations studentOps = new StudentOperations();
+
+        // Display menu
+        Menu.displayMenu(studentOps);
+    }
+}
